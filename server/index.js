89646a6f199eedce1277c3f5480031b656045285
Advay Aggarwal <advayaggarwal0301@gmail.com>
2022-05-03 14:07:14 +0530

    Add login signup

---
+++ server/index.js
@@ -0,0 +1,54 @@
+const express = require("express"),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser"),
+  User = require("./models/User"),
+  app = express(),
+  cors = require("cors"),
+  PORT = process.env.PORT || 4500;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/RBAC", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Mongo.."))
+  .catch((err) => console.log("Error\n %s", err));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Server running" });
+});
+app.post("/signup", async (req, res) => {
+  try {
+    const newUser = await new User(req.body);
+    await newUser.save();
+    return res.send({ status: 200, message: "new User created successfully" });
+  } catch (err) {
+    return res.status(500).send({
+      error: err,
+    });
+  }
+});
+
+app.get("/userInfo", async (req, res) => {
+  const users = await User.find();
+  res.json({ users_list: users });
+});
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.exists({ email, password });
+  if (user) return res.send({ status: 200, data: { email, password } });
+  return res.status(404).send({
+    message: "Email or Password is incorrect",
+  });
+});
+app.listen(PORT, () => {
+  console.log("Server listening at port:-", PORT);
+});
